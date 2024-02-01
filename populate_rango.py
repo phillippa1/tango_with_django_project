bdd84919eaec369bb70092194151d103b4ffee10
“phillippa1” <phillippa.hamilton@gmail.com>
2024-02-01 23:12:03 +0000

    1

--- populate_rango.py
+++ populate_rango.py
@@ -32,14 +32,15 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
     
-    cats = {'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+    
     
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], cat_data['views'])
 
 
     for c in Category.objects.all():
@@ -51,11 +52,13 @@ def add_page(cat, title, url, views=0):
         p = Page.objects.get_or_create(category=cat, title=title)[0]
         p.url=url
         p.views=views
+        p.likes = cat.likes
         p.save()
         return p
 
 def add_cat(name):
         c = Category.objects.get_or_create(name=name)[0]
+        c.likes = 0
         c.save()
         return c
 
